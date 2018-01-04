0cbc6d43faf82a45385227cf798812b606af1e78
Fred Kim <yhk1038@hanmail.net>
2018-01-05 01:34:42 +0900

    Add Top-topic and navbar and carousel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    $appname = Rails.application.class.parent_name
+
 end
